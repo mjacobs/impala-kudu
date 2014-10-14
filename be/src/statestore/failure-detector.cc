1bb3066dfe4b3cb4060d4d5e019e0e22e3f2f16f
Todd Lipcon <todd@cloudera.com>
2014-10-14 11:49:22 -0700

    build fixes to allow a BOOST_ROOT different than the system install

--- be/src/statestore/failure-detector.cc
+++ be/src/statestore/failure-detector.cc
@@ -15,8 +15,7 @@
 #include "statestore/failure-detector.h"
 
 #include <boost/assign.hpp>
-#include <boost/thread.hpp>
-
+#include <boost/thread/locks.hpp>
 #include "common/logging.h"
 
 using namespace impala;
