9c245fe009c4121200fa3d0fef66b75869063b0c
casey <casey@cloudera.com>
2016-01-27 11:24:47 -0800

    Get and use Kudu from the toolchain by default
    This is for review purposes only. This patch will be merged with David's
    big merge patch.
    
    Changes:
    1) Make Kudu compilation dependent on the OS since not all OSs support
       Kudu.
    2) Only run Kudu related tests when Kudu is supported (see #1).
    3) Look for Kudu locally, but in a different location. To use a local
       build of Kudu, set KUDU_BUILD_DIR to the path Kudu was built in and
       set KUDU_CLIENT_DIR to the path KUDU was installed in.
       Example:
         git clone https://github.com/cloudera/kudu.git
         ...build 3rd party etc...
         mkdir -p $KUDU_BUILD_DIR
         cd $KUDU_BUILD_DIR
         cmake <path to Kudu source dir>
         make
         DESTDIR=$KUDU_CLIENT_DIR make install
    4) Look for Kudu in the toolchain if not using a local Kudu build.
    5) Add Kudu service startup scripts. The Kudu in the toolchain is
       actually a parcel that has been renamed (the contents were not
       modified in any way), that mean the Kudu service binaries are there.
       Those binaries are now used to run the Kudu service.
    
    Change-Id: I3db88cbd27f2ea2394f011bc8d1face37411ed58

--- be/src/exec/kudu-scanner.cc
+++ be/src/exec/kudu-scanner.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifdef USE_KUDU
+
 #include "exec/kudu-scanner.h"
 
 #include <boost/foreach.hpp>
@@ -344,3 +346,5 @@ Status KuduScanner::GetNextBlock() {
 }
 
 }  // namespace impala
+
+#endif
